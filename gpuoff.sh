addd8aa78caa1957a35a95dfee5b7ed64b1d2b96
Cole Deck <cole@deck.sh>
2020-08-07 23:36:02 -0500

    Fix separate GPU off script

--- gpuoff.sh
+++ gpuoff.sh
@@ -7,12 +7,11 @@
 #sudo sh -c "echo 1 > /sys/bus/pci/devices/0000:01:00.0/remove; echo '\\_SB.PCI0.PEG0.PEGP._OFF' >     /proc/acpi/call"
 #output=$(optimus-manager --print-mode)
 #echo $output
-if [ $(sudo -u cole xrandr --listproviders | grep NVIDIA) ]; then
+if xrandr --listproviders | grep NVIDIA; then
 	echo "GPU is in use for PRIME, keeping on"
 else
 	sudo sh -c 'echo "\\_SB.PCI0.GPP0.PG00._OFF" > /proc/acpi/call'
-        echo $(sudo cat /proc/acpi/call)
-        echo "GPU POWERED OFF"
+        echo "GPU powered off"
 fi  
 #if [ "$(optimus-manager --print-mode)" = "
 #
